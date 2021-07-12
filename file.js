4b9aeb0f9290bb02cad81d7dee4ff631e5c88404
Collins Adom Baffour <collins.baffour@amalitech.org>
2021-07-12 19:08:32 +0000

    Update

--- file.js
+++ file.js
@@ -24,13 +24,31 @@ const fs = require('fs');
 
 
 //directories
-fs.mkdir('./assets', (err) => {
-    if (err) {
-        console.log(err);
-    }
-    console.log('folder created')
-})
+if (!fs.existsSync('./assets')) {
+
+    fs.mkdir('./assets', (err) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log('folder created')
+    });
+} else {
+    fs.rmdir('./assets', (err) => {
+        if (err) {
+            console.log('err')
+        }
+        console.log('folder deleted')
+    });
+}
 
 
 
 // deleting files
+if (fs.existsSync('./docs/delete.txt')) {
+    fs.unlink('./docs/delete.txt', (err) => {
+        if (err) {
+            console.log('err')
+        }
+        console.log('file deleted')
+    })
+}
